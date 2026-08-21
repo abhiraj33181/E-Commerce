0ee3da4a032516d8955d26a02c866d373f43cd1d
Abhishek Raj <abhishekraj995555@gmail.com>
2026-08-21 15:23:36 +0530

    Cart Screen UI Completed

--- client/app/(tabs)/Cart.tsx
+++ client/app/(tabs)/Cart.tsx
@@ -1,10 +1,73 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, ScrollView } from 'react-native'
 import React from 'react'
+import { useCart } from '@/context/CartContext'
+import { useRouter } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Header from '@/components/Header';
+import CartItem from '@/components/CartItem';
 
 export default function Cart() {
+
+  const { cartItems, cartTotal, removeFromCart, clearCart, updateQuantity } = useCart();
+  const router = useRouter();
+
+  const shipping = 2.00;
+  const total = cartTotal + shipping;
+
   return (
-    <View>
-      <Text>Cart</Text>
-    </View>
+    <SafeAreaView className='flex-1 bg-surface' edges={['top']}>
+      <Header title='My Cart' showBack />
+
+      {cartItems.length > 0 ? (
+        <>
+          <ScrollView className='flex-1 px-4 mt-1' showsVerticalScrollIndicator={false}>
+            {cartItems.map((item, index) => (
+              <CartItem
+                item={item}
+                key={index}
+                onRemove={() => removeFromCart(item.id, item.size)}
+                onUpdateQuantity={() => updateQuantity(item.id, 5, item.size)}
+              />
+            ))}
+          </ScrollView>
+
+          <View className='p-4 bg-white rounded-t-3xl shadow-sm'>
+            {/* SubTotal */}
+            <View className='flex-row justify-between mb-2'>
+              <Text className='text-secondary'>Sub Total</Text>
+              <Text className='text-primary font-bold'>${cartTotal.toFixed(2)}</Text>
+            </View>
+
+            {/* Shipping Details */}
+            <View className='flex-row justify-between mb-2'>
+              <Text className='text-secondary'>Shipping</Text>
+              <Text className='text-primary font-bold'>${shipping.toFixed(2)}</Text>
+            </View>
+
+            {/* border */}
+            <View className='h-[1px] bg-border mb-4' />
+
+            {/* Total */}
+            <View className='flex-row justify-between mb-6'>
+              <Text className='text-primary font-bold text-lg'>Total</Text>
+              <Text className='text-primary font-bold text-lg'>${total.toFixed(2)}</Text>
+            </View>
+
+
+            {/* Checkout */}
+            <TouchableOpacity className='bg-primary py-4 rounded-full items-center' onPress={() => router.push("/checkout")}>
+              <Text className='text-white font-bold text-base'>Chechout</Text>
+            </TouchableOpacity>
+          </View>
+        </>
+      ) : (
+        <View className='flex-1 items-center justify-center'>
+          <Text className='text-secondary text-lg'>Your Cart is empty</Text>
+          <TouchableOpacity onPress={() => router.push('/')} className='rounded mt-4 bg-primary'>
+            <Text className='p-2 text-white font-bold'>Start Shopping</Text>
+          </TouchableOpacity>
+        </View>
+      )}
+    </SafeAreaView>
   )
 }
--- client/app/(tabs)/_layout.tsx
+++ client/app/(tabs)/_layout.tsx
@@ -1,25 +1,30 @@
 import React from 'react'
 import { Tabs } from 'expo-router'
 import { Feather, Ionicons } from '@expo/vector-icons'
-import {COLORS} from '@/constants'
+import { COLORS } from '@/constants'
+import { useCart } from '@/context/CartContext'
+import { View } from 'react-native'
 
 export default function TabLayout() {
+
+    const { cartItems } = useCart();
+
     return (
         <Tabs
             screenOptions={{
-                headerShown : false,
-                tabBarActiveTintColor : COLORS.primary,
-                tabBarInactiveTintColor : '#CDCDE0',
-                tabBarShowLabel : false,
-                tabBarStyle : {
-                    backgroundColor : '#fff',
-                    borderTopWidth : 1,
-                    borderTopColor : '#F0F0F0',
-                    height : 64,
-                    paddingTop : 8,
-                    paddingBottom : 8
+                headerShown: false,
+                tabBarActiveTintColor: COLORS.primary,
+                tabBarInactiveTintColor: '#CDCDE0',
+                tabBarShowLabel: false,
+                tabBarStyle: {
+                    backgroundColor: '#fff',
+                    borderTopWidth: 1,
+                    borderTopColor: '#F0F0F0',
+                    height: 64,
+                    paddingTop: 8,
+                    paddingBottom: 8
                 }
-                
+
             }}
         >
             <Tabs.Screen
@@ -33,12 +38,25 @@ export default function TabLayout() {
             <Tabs.Screen
                 name='Cart'
                 options={{
-                    tabBarIcon: ({ color, focused }) => <Feather name={focused ? 'shopping-cart' : 'shopping-cart'} size={26} color={color} />
+                    tabBarIcon: ({ color, focused }) => (
+                        <View className='relative'>
+                            <Feather name={focused ? 'shopping-cart' : 'shopping-cart'} size={26} color={color} />
+
+                            {
+                                cartItems.length > 0 && (
+                                    <View className='absolute -top-2 -right-2 bg-accent size-3 rounded-full items-center justify-center'>
+                                        <Ionicons name='ellipse' size={6} color="white" />
+                                    </View>
+                                )
+                            }
+
+                        </View>
+                    )
                 }}
             />
 
             {/* Favourite */}
-           <Tabs.Screen
+            <Tabs.Screen
                 name='Favourites'
                 options={{
                     tabBarIcon: ({ color, focused }) => <Ionicons name={focused ? 'heart' : 'heart-outline'} size={26} color={color} />
